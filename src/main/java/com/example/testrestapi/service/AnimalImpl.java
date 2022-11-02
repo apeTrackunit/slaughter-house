4f479a1abf9862e9f359d1313d29a42f692b040a
apeTrackunit <ape@trackunit.com>
2022-11-02 16:57:06 +0100

    AnimalImpl

--- src/main/java/com/example/testrestapi/service/AnimalImpl.java
+++ src/main/java/com/example/testrestapi/service/AnimalImpl.java
@@ -3,25 +3,31 @@ package com.example.testrestapi.service;
 import com.example.testrestapi.AnimalsServiceGrpc;
 import com.example.testrestapi.GetAnimalIdsRequest;
 import com.example.testrestapi.GetAnimalIdsResponse;
-import com.example.testrestapi.entity.AnimalPart;
-import com.example.testrestapi.entity.Product;
+import com.example.testrestapi.dbConnection.DBAnimal;
+import com.example.testrestapi.entity.Animal;
 import io.grpc.stub.StreamObserver;
+
 import java.util.List;
 
 public class AnimalImpl extends AnimalsServiceGrpc.AnimalsServiceImplBase {
 
     @Override
     public void getAnimalIds(GetAnimalIdsRequest request, StreamObserver<GetAnimalIdsResponse> responseObserver) {
-        Product product = null;
-        List<AnimalPart> animalParts = product.getAnimalParts();
+        try{
 
-        GetAnimalIdsResponse.Builder builder = GetAnimalIdsResponse.newBuilder();
-        for (int i = 0; i < animalParts.size(); i++) {
-            builder.addAnimalIds(animalParts.get(i).getAnimal().getId());
+            List<Animal> animals = DBAnimal.getAnimalsByProductId(request.getProductId());
+
+            GetAnimalIdsResponse.Builder builder = GetAnimalIdsResponse.newBuilder();
+            for (int i = 0; i < animals.size(); i++) {
+                builder.addAnimalIds(animals.get(i).getId());
+            }
+
+            GetAnimalIdsResponse responseText = builder.build();
+            responseObserver.onNext(responseText);
+            responseObserver.onCompleted();
         }
-
-        GetAnimalIdsResponse responseText = builder.build();
-        responseObserver.onNext(responseText);
-        responseObserver.onCompleted();
+        catch (Exception ex){
+            System.out.println(ex.getStackTrace());
+        }
     }
 }
